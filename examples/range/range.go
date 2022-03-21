5410281186e9c3b93e89036d4e2e74b7d64dc1b8
Wade <nkb@gabia.com>
2022-03-21 15:27:33 +0900

    Initial range.go

---
+++ examples/range/range.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// range는 array, slice, map 등을 순회
+	a := []string{"a", "b", "c", "d", "e"}
+	for idx, val := range a {
+		fmt.Println(idx, val) // 0 a, 1 b, 2 c, 3 d, 4 e
+	}
+
+	b := map[string]string{
+		"apple": "fruit",
+		"onion": "vegetable",
+	}
+	for key, val := range b {
+		fmt.Println(key, val) // apple fruit, onion vegetable
+	}
+
+	for key := range b {
+		fmt.Println(key) // apple, onion
+	}
+}
