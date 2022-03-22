ba5871cbf75dedc1d0a17d6d9a04606d852b3b23
Wade <nkb@gabia.com>
2022-03-22 15:18:59 +0900

    Modify: 슬라이스 append 예시 추가

--- examples/slice/slice.go
+++ examples/slice/slice.go
@@ -12,6 +12,10 @@ func main() {
 	a = append(a, 6) // 원소 추가
 	fmt.Println(a)   // [1 2 3 4 5 6]
 
+	var tmp = []int{7, 8, 9, 10}
+	a = append(a, tmp...) // 슬라이스 추가
+	fmt.Println(a)        // [1 2 3 4 5 6 7 8 9 10]
+
 	b := make([]string, 5) // 길이가 5인 빈 문자열을 원소로 가지고 있는 slice 생성
 	fmt.Println(b)         // [   ]
 
