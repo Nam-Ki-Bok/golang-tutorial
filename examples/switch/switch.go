82a183f062a9d030c06aa47ae39fff791088cf38
Wade <nkb@gabia.com>
2022-03-22 14:30:50 +0900

    Modify: switch 예시 추가

--- examples/switch/switch.go
+++ examples/switch/switch.go
@@ -18,6 +18,24 @@ func main() {
 		fmt.Println("Who am I?")
 	}
 
+	// switch 뒤에 생략 가능
+	// case 뒤에 식 대입 가능
+	score := 47
+	func(score int) {
+		switch {
+		case score >= 90:
+			println("A")
+		case score >= 80:
+			println("B")
+		case score >= 70:
+			println("C")
+		case score >= 60:
+			println("D")
+		default:
+			println("No Hope")
+		}
+	}(score)
+
 	// fallthrough 사용 시
 	// 해당 case 밑에 있는 모든 case 실행
 	switch num := 1; num {
